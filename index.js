5e196d447de0a1329ba17439e1dd787488dbde9a
flakegotgame <victorchange808@gmail.com>
2024-04-03 22:16:49 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+// Write your code here!main.remove()
+main.remove()
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerHTML = "Victor is the champion";
