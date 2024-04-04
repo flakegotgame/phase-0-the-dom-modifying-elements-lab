6b2de5a06501c63d457ff350172db0db229f0dd6
flakegotgame <victorchange808@gmail.com>
2024-04-04 10:06:19 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-// Write your code here!main.remove()
-main.remove()
-const newHeader = document.createElement("h1");
-newHeader.id = "victory";
-newHeader.innerHTML = "Victor is the champion";
+// Write your code here!
+const mainElement = document.getElementById('main');
+mainElement.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+
+const yourName = 'John Doe'; 
+newHeader.textContent = `${yourName} is the champion`;
+
+document.body.appendChild(newHeader);
+
